4752a30833aa0e3bd9c98ddf9a2f666ddd780641
Kojie-ignacio <106798673+Kojie-ignacio@users.noreply.github.com>
2022-06-23 22:41:51 +0800

    Add files via upload

---
+++ OE5_OOP/EmployeeDriver.java
@@ -0,0 +1,11 @@
+package OE5_OOP;
+
+public class EmployeeDriver {
+
+    public static void main(String[] args) {
+
+        Employee emp = new Employee();
+        System.out.println(emp);
+    }
+
+}
